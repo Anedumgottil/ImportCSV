979d81627c7f159a0b6377cb456c49d041a0340b
Anthony Nedumgottil <anedumgottil@gmail.com>
2014-07-21 11:26:30 -0500

    Added methods for the average shortest path

--- Source1.cpp
+++ Source1.cpp
@@ -12,7 +12,9 @@ More documentation on vectors can be found here http://www.cplusplus.com/referen
 #include <vector>
 #include <fstream>
 #include <numeric>
+#include <climits>
 
+#define V 9
 using namespace std;
 /*
 changeMatrix: Changes the matrix depending if the county is within a certain % of eachother
@@ -105,6 +107,68 @@ void calculateDegree(int** matrix, vector<int> &degree, int num_of_entities){
 		degree.push_back(sum);
 		sum = 0;
 	}
+}
+
+int minDistance(int dist[], bool sptSet[], int num_of_entities)
+{
+	// Initialize min value
+	int min = INT_MAX, min_index;
+
+	for (int v = 0; v < num_of_entities; v++)
+		if (sptSet[v] == false && dist[v] <= min)
+			min = dist[v], min_index = v;
+
+	return min_index;
+}
+
+// A utility function to print the constructed distance array
+int printSolution(int dist[], int num_of_entities)
+{
+	printf("Vertex   Distance from Source\n");
+	for (int i = 0; i < num_of_entities; i++)
+		printf("%d \t\t %d\n", i, dist[i]);
+}
+
+// Funtion that implements Dijkstra's single source shortest path algorithm
+// for a graph represented using adjacency matrix representation
+void dijkstra(int** matrix, int src, int num_of_entities)
+{
+	int dist[num_of_entities];     // The output array.  dist[i] will hold the shortest
+	// distance from src to i
+
+	bool sptSet[num_of_entities]; // sptSet[i] will true if vertex i is included in shortest
+	// path tree or shortest distance from src to i is finalized
+
+	// Initialize all distances as INFINITE and stpSet[] as false
+	for (int i = 0; i < num_of_entities; i++)
+		dist[i] = INT_MAX, sptSet[i] = false;
+
+	// Distance of source vertex from itself is always 0
+	dist[src] = 0;
+
+	// Find shortest path for all vertices
+	for (int count = 0; count < num_of_entities - 1; count++)
+	{
+		// Pick the minimum distance vertex from the set of vertices not
+		// yet processed. u is always equal to src in first iteration.
+		int u = minDistance(dist, sptSet);
+
+		// Mark the picked vertex as processed
+		sptSet[u] = true;
+
+		// Update dist value of the adjacent vertices of the picked vertex.
+		for (int v = 0; v < num_of_entities; v++)
+
+			// Update dist[v] only if is not in sptSet, there is an edge from 
+			// u to v, and total weight of path from src to  v through u is 
+			// smaller than current value of dist[v]
+			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
+				&& dist[u] + graph[u][v] < dist[v])
+				dist[v] = dist[u] + graph[u][v];
+	}
+
+	// print the constructed distance array
+	printSolution(dist, num_of_entities);
 }
 
 int main(){
